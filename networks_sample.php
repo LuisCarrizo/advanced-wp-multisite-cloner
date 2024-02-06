f4caac118ce1d4ac4137814a7d02b311b2c2407a
LuisCarrizo <lcarrizo2012@gmail.com>
2024-02-06 18:17:52 -0300

    martes 2

---
+++ networks_sample.php
@@ -0,0 +1,52 @@
+<?php 
+$networks = array(
+	'host' => array(
+		// host
+		array(
+			'name'   => 'Wikired @host ',
+			'url'    => 'https://wikired.com.ar/',
+			'folder' => 'public_html/',
+			'main'   => 'https://wikired.com.ar/'),
+		array(
+			'name'   => 'Demos @host',
+			'url'    => 'https://demo.wikired.com.ar/',
+			'folder' => 'wr.demo/',
+			'main'   => 'https://demo.wikired.com.ar'),
+		array(
+			'name'   => 'CRP @host',
+			'url'    => 'https://crp.wikired.com.ar/',
+			'folder' => 'wr.crp/',
+			'main'   => 'https://crp.wikired.com.ar'),
+	),
+	'home' => array(
+		// localhost at home
+		array(
+			'name'   => 'Wikired @home',
+			'url'    => 'http://wikired.net/',
+			'folder' => 'wp_wr/',
+			'main'   => 'http://wikired.net/'),
+		array(
+			'name'   => 'CRP @home',
+			'url'    => 'http://crp.net/',
+			'folder' => 'wp_crp/',
+			'main'   => 'http://wikired.net/'),
+	),
+	'work' => array(
+		// localhost at work
+		array(
+			'name'   => 'Wikired @work',
+			'url'    => 'http://wikired.net/',
+			'folder' => 'wpmain/',
+			'main'   => 'http://wikired.net/'),
+		array(
+			'name'   => 'CRP @work',
+			'url'    => 'http://crp.net/',
+			'folder' => 'wpcrp/',
+			'main'   => 'http://wikired.net/'),
+		array(
+			'name'   => 'Test @work',
+			'url'    => 'http://wptest.net/',
+			'folder' => 'wptest/',
+			'main'   => 'http://wikired.net/'),
+	),
+);
